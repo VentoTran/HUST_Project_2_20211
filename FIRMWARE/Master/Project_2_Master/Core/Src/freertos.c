73e28186568e07c743d17bd63a7d2839b8ad30e0
Vento <zseefvhu12345@gmail.com>
2022-02-10 15:24:56 +0700

    Ven

--- FIRMWARE/Master/Project_2_Master/Core/Src/freertos.c
+++ FIRMWARE/Master/Project_2_Master/Core/Src/freertos.c
@@ -733,15 +733,15 @@ void InfoPageHandler(uint16_t x, uint16_t y)
 
 void reverse(char* str, int len)
 {
-    int i = 0, j = len - 1, temp;
-    while (i < j)
-    {
-        temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
-        i++;
-        j--;
-    }
+  int i = 0, j = len - 1, temp;
+  while (i < j)
+  {
+      temp = str[i];
+      str[i] = str[j];
+      str[j] = temp;
+      i++;
+      j--;
+  }
 }
 
 // Converts a given integer x to string str[]. 
@@ -750,45 +750,45 @@ void reverse(char* str, int len)
 // then 0s are added at the beginning.
 int intToStr(uint8_t x, char str[], int d)
 {
-    int i = 0;
-    if (x == 0)
+  int i = 0;
+  if (x == 0)
+  {
+    str[i++] = '0';
+  }
+  else
+  {
+    while (x)
     {
-      str[i++] = '0';
-    }
-    else
-    {
-      while (x)
-      {
-        str[i++] = (x % 10) + '0';
-        x = x / 10;
-      } 
-    }
-    // If number of digits required is more, then
-    // add 0s at the beginning
-    while (i < d)
-        str[i++] = ' ';
+      str[i++] = (x % 10) + '0';
+      x = x / 10;
+    } 
+  }
+  // If number of digits required is more, then
+  // add 0s at the beginning
+  while (i < d)
+    str[i++] = ' ';
 
-    reverse(str, i);
-    str[i] = '\0';
-    return i;
+  reverse(str, i);
+  str[i] = '\0';
+  return i;
 }
 
 // Converts a floating-point/double number to a string.
 void ftoa(float n, char* res, int afterpoint)
 {
-    // Extract integer part
-    int ipart = (int)n;
-    // Extract floating part
-    float fpart = n - (float)ipart;
-    // convert integer part to string
-    int i = intToStr(ipart, res, 3);
-    // check for display option after point
-    if (afterpoint != 0)
-    {
-        res[i] = '.'; // add dot
-        fpart = fpart * pow(10, afterpoint);
-        intToStr((int)fpart, res + i + 1, afterpoint);
-    }
+  // Extract integer part
+  int ipart = (int)n;
+  // Extract floating part
+  float fpart = n - (float)ipart;
+  // convert integer part to string
+  int i = intToStr(ipart, res, 3);
+  // check for display option after point
+  if (afterpoint != 0)
+  {
+    res[i] = '.'; // add dot
+    fpart = fpart * pow(10, afterpoint);
+    intToStr((int)fpart, res + i + 1, afterpoint);
+  }
 }
 
 void intToTime(uint32_t time, char* str)
@@ -806,27 +806,27 @@ void intToTime(uint32_t time, char* str)
 
 int intToStr0(uint8_t x, char str[], int d)
 {
-    int i = 0;
-    if (x == 0)
+  int i = 0;
+  if (x == 0)
+  {
+    str[i++] = '0';
+  }
+  else
+  {
+    while (x)
     {
-      str[i++] = '0';
-    }
-    else
-    {
-      while (x)
-      {
-        str[i++] = (x % 10) + '0';
-        x = x / 10;
-      } 
-    }
-    // If number of digits required is more, then
-    // add 0s at the beginning
-    while (i < d)
-        str[i++] = '0';
+      str[i++] = (x % 10) + '0';
+      x = x / 10;
+    } 
+  }
+  // If number of digits required is more, then
+  // add 0s at the beginning
+  while (i < d)
+    str[i++] = '0';
 
-    reverse(str, i);
-    str[i] = '\0';
-    return i;
+  reverse(str, i);
+  str[i] = '\0';
+  return i;
 }
 
 /* USER CODE END Application */
